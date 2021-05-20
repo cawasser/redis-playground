b1568016aa876bae4acc2b08e3eeab91d5873c7b
Chris Wasser <cawasser@mac.com>
2021-05-20 15:43:42 -0400

    - baseline from clj-new

---
+++ test/redis_playground/redis_playground_test.clj
@@ -0,0 +1,7 @@
+(ns redis-playground.redis-playground-test
+  (:require [clojure.test :refer :all]
+            [redis-playground.redis-playground :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
